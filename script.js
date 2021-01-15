14c3dc6ef43b9bfd34951079cb491f6be8c7c361
spencercreer <spencercreer@gmail.com>
2021-01-15 13:58:32 -0700

    store textarea values to local storage upon button click

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
-    setInterval(currentTime,1000)
+
+// Get items from localStorage for each hour
+function init(){
+    let dt = new Date();
+    let todaysDate = `${
+      dt.getMonth() + 1
+    } / ${dt.getDate()} / ${dt.getFullYear()}`;
+
+    storedDate = localStorage.getItem("today");
     
-dateEl = document.querySelector("#currentDay");
+    // if storedDate equals todaysDate, get all localStorage elements and load into textarea value
+    if(storedDate === todaysDate){
+        $("textarea").each(function(){
+            let noteHour = $(this).prev().text();
+            $(this).val(localStorage.getItem(noteHour))
+        })
+    // else clear all localStorage and store todaysDate
+    } else{
+        localStorage.clear();
+        localStorage.setItem("today", todaysDate);
+    }
+}
 
-// Display date to currentDay
-// dateEl.textContent = todayDate;
+setInterval(currentTime,1000)
 
-// Update time blocks color to todayTime
-
-// Get current time
+// Get current time update time blocks
 function currentTime(){
     let dt = new Date();
     let todaysDate = `${
       dt.getMonth() + 1
     } / ${dt.getDate()} / ${dt.getFullYear()}`;
+
     $("#currentDay").text(todaysDate);
     let currentHour = dt.getHours();
     let currentTime = dt.toLocaleTimeString();
@@ -21,18 +38,18 @@ function currentTime(){
 
     $("textarea").each(function(){
         var blockTime = parseInt($(this).attr("id"));
-        
-        // console.log(blockTime)
+
         // Check if past
         if(blockTime < currentHour){
+            $(this).removeClass("present")
             $(this).addClass("past");
         // Check if present
         } else if(currentHour === blockTime){
-            $(this).removeClass("future")
+            $(this).removeClass("future");
             $(this).addClass("present");
         // Check if future
         } else {
-            $(this).removeClass("past")
+            $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
         }
@@ -40,6 +57,12 @@ function currentTime(){
 };
 
 // On button click, save textarea content to localStorage
+$(".saveBtn").on("click", function(){
+    let notes = $(this).prev().val();
+    let noteHour = $(this).siblings(".hour").text()
+    localStorage.setItem(noteHour, notes);
+});
 
+init();
 currentTime();
 });
