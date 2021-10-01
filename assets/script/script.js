bb57da4a6182ab33576d951ea2a6b84a8733aa0c
spencercreer <spencercreer@gmail.com>
2021-09-30 23:21:02 -0700

    updated var to let and added moment.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,117 +1,120 @@
-$(document).ready(function() {
-    // Month and weekday array
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var time = ["6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm"]
-    var buttonText = $(".swapStyle")
+$(document).ready(function () {
+    let buttonText = $(".swapStyle")
 
-function init(){
-    // Get date using JavaScript date methods
-    let dt = new Date();
-    let todaysDate = `${
-      dt.getMonth() + 1
-    } / ${dt.getDate()} / ${dt.getFullYear()}`;
-
-
-    // Create timeblocks using template literals
-    for (let i = 0; i < time.length; i++) {
-        const timeblock = `
-        <div class="row">
-            <div class="col-2 hour">${time[i]}</div>
-            <textarea class="col-8" id="${6+i}" rows="2"></textarea>
-            <button class="col-2 saveBtn far fa-save"></button>
-        </div>
-        `;
-        $("#time-blocks").append(timeblock);
+    function main() {
+        setInterval(currentTime, 1000)
+        displayTimeBlocks()
+        getLocalStorageStyle()
+        getLocalStorageText()
     }
 
-    // Get items from localStorage for each hour
-    var storedDate = localStorage.getItem("today");
-    if(!localStorage.getItem("Style")){
-        localStorage.setItem("Style","assets/style/style.css")
+    // Get current time update time blocks
+    function currentTime() {
+        displayDate()
+        updateTimeBlocks()
+    };
+
+    function displayDate() {
+               // Get date, day, and time using moment.js
+               let todaysDate = moment().format('MMMM Do YYYY');
+               let weekDay = moment().format('dddd')
+               let currentTime = moment().format('LT');
+       
+               $("#currentTime").text(currentTime);
+               $("#currentDay").text(todaysDate);
+               $("#weekDay").text(weekDay);
     }
-    var storedStyle = localStorage.getItem("Style");
-    if(storedStyle === "assets/style/style.css"){
-        buttonText.text("Light Mode")
-    } else{
-        buttonText.text("Dark Mode")
-    }
 
-    $("#pagestyle").attr("href",storedStyle);
-    
-    // if storedDate equals todaysDate, get all localStorage elements and load into textarea value
-    if(storedDate === todaysDate){
-        $("textarea").each(function(){
-            let noteHour = $(this).prev().text();
-            $(this).val(localStorage.getItem(noteHour))
-        })
-    // else clear all localStorage and store todaysDate
-    } else{
-        localStorage.clear();
-        localStorage.setItem("today", todaysDate);
-    }
-}
-
-setInterval(currentTime,1000)
-
-// Get current time update time blocks
-function currentTime(){
-    let dt = new Date();
-    let day = dt.getDate();
-    let weekDay = weekdays[dt.getDay()]
-    let month = months[dt.getMonth()];
-    let year = dt.getFullYear();
-    let todaysDate = day + " " + month + " " + year;
-
-    $("#currentDay").text(todaysDate);
-    $("#weekDay").text(weekDay);
-
-    let currentHour = dt.getHours();
-    let currentTime = dt.toLocaleTimeString();
-    document.getElementById("currentTime").innerHTML = currentTime;
-
-    $("textarea").each(function(){
-        var blockTime = parseInt($(this).attr("id"));
-
-        // Check if past
-        if(blockTime < currentHour){
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        // Check if present
-        } else if(currentHour === blockTime){
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        // Check if future
-        } else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
+    function displayTimeBlocks() {
+        let time = ["6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm"]
+        // Create timeblocks using template literals
+        for (let i = 0; i < time.length; i++) {
+            const timeblock = `
+            <div class="row">
+                <div class="col-2 hour">${time[i]}</div>
+                <textarea class="col-8" id="${6 + i}" rows="2"></textarea>
+                <button class="col-2 saveBtn far fa-save"></button>
+            </div>
+            `;
+            $("#time-blocks").append(timeblock);
         }
-    });
-};
-
-// On button click, save textarea content to localStorage
-$(".saveBtn").on("click", function(){
-    let notes = $(this).prev().val();
-    let noteHour = $(this).siblings(".hour").text()
-    localStorage.setItem(noteHour, notes);
-});
-
-// On change style button click, change style
-$(".swapStyle").on("click",function(){
-    let style = $("#pagestyle").attr("href")
-    if(style === "assets/style/style.css"){
-        $("#pagestyle").attr("href","assets/style/dark_style.css");
-        buttonText.text("Dark Mode")
-        localStorage.setItem("Style", "assets/style/dark_style.css");
-    } else{
-        $("#pagestyle").attr("href","assets/style/style.css");
-        buttonText.text("Light Mode")
-        localStorage.setItem("Style", "assets/style/style.css");
     }
-})
 
-currentTime();
-init();
+    function updateTimeBlocks() {
+        $("textarea").each(function () {
+            let currentHour = moment().hour();
+            let blockTime = parseInt($(this).attr("id"));
+
+            // Check if past
+            if (blockTime < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                // Check if present
+            } else if (currentHour === blockTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                // Check if future
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    function getLocalStorageStyle() {
+        let storedStyle = localStorage.getItem("Style");
+        // Get items from localStorage for each hour
+        if (!localStorage.getItem("Style")) {
+            localStorage.setItem("Style", "assets/style/style.css")
+        }
+        if (storedStyle === "assets/style/style.css") {
+            buttonText.text("Light Mode")
+        } else {
+            buttonText.text("Dark Mode")
+        }
+
+        $("#pagestyle").attr("href", storedStyle);
+    }
+
+    function getLocalStorageText() {
+        // if storedDate equals todaysDate, get all localStorage elements and load into textarea value
+        let storedDate = localStorage.getItem("today");
+        let todaysDate = moment().format('MMMM Do YYYY');
+        if (storedDate === todaysDate) {
+            $("textarea").each(function () {
+                let noteHour = $(this).prev().text();
+                $(this).val(localStorage.getItem(noteHour))
+            })
+        } else {
+            // clear all localStorage and store todaysDate
+            localStorage.clear();
+            localStorage.setItem("today", todaysDate);
+        }
+    }
+
+    // On button click, save textarea content to localStorage
+    $(".saveBtn").on("click", function () {
+        let notes = $(this).prev().val();
+        let noteHour = $(this).siblings(".hour").text()
+        localStorage.setItem(noteHour, notes);
+    });
+
+    // On change style button click, change style
+    $(".swapStyle").on("click", function () {
+        let style = $("#pagestyle").attr("href")
+        if (style === "assets/style/style.css") {
+            $("#pagestyle").attr("href", "assets/style/dark_style.css");
+            buttonText.text("Dark Mode")
+            localStorage.setItem("Style", "assets/style/dark_style.css");
+        } else {
+            $("#pagestyle").attr("href", "assets/style/style.css");
+            buttonText.text("Light Mode")
+            localStorage.setItem("Style", "assets/style/style.css");
+        }
+    })
+
+    main();
 });
